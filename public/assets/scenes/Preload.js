f73fcb79b9257bd96eb35f5c78a9b8e9a39fe4f3
Ian <110197575+Ian0303@users.noreply.github.com>
2023-06-27 16:56:45 -0300

    funcional

--- public/assets/scenes/Preload.js
+++ public/assets/scenes/Preload.js
@@ -64,7 +64,7 @@ create(){
   this.add.image(400, 300, "background2");
   this.add.image(400, 300, "title");
   this.add.image(400, 300, "loading").setInteractive()
-.on('pointerdown', () => this.scene.start("Level1")); ;
+.on('pointerdown', () => this.scene.start("Menu")); ;
   
    /*  this.time.addEvent({
       delay: 6000,
